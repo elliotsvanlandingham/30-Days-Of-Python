b184e189b0c7219c3feda8310625c6b972877c50
elliotsvanlandingham <elliotsvanlandingham@gmail.com>
2026-06-03 12:53:42 -0500

    Update variables.py

--- day_2/variables.py
+++ day_2/variables.py
@@ -1,1 +1,32 @@
+'Day 2: 30 Days of python programming'
+# Variables in Python
+
+first_name = 'Elliot'
+last_name = 'VanLandingham'
+full_name = f"{first_name} {last_name}"
+country = 'USA'
+city = 'Anna'
+age = 29
+year = 2026
+is_married = 'yes'
+is_true = True
+is_light_on = 'yes'
+personal_info = {
+    'first_name': first_name,
+    'last_name': last_name,
+    'full_name': full_name,
+    'country': country,
+    'city': city,
+    'age': age,
+    'year': year,
+    'is_married': is_married,
+}
+first_name, last_name, country, city = 'Elliot', 'VanLandingham', 'USA', 'Anna'
+age, year = 29, 2026
+full_name = "f{first_name} {last_name}"
+
+# Check data type for all variables
+
+print(type(first_name)
+# Print values stored in Variables
 
