ff76e91c41a63dfb0e009b867ac892532ed2b36d
Elliot VanLandingham <elliotsvanlandingham@gmail.com>
2026-06-04 14:17:34 -0500

    Finished Day 2 exercises

--- day_2/variables.py
+++ day_2/variables.py
@@ -1,6 +1,9 @@
 'Day 2: 30 Days of python programming'
 # Variables in Python
 
+from os import remove, replace
+
+
 first_name = 'Elliot'
 last_name = 'VanLandingham'
 full_name = f"{first_name} {last_name}"
@@ -23,10 +26,33 @@ personal_info = {
 }
 first_name, last_name, country, city = 'Elliot', 'VanLandingham', 'USA', 'Anna'
 age, year = 29, 2026
-full_name = "f{first_name} {last_name}"
+full_name = f"{first_name} {last_name}"
 
 # Check data type for all variables
 
 print(type(first_name))
+print(type(last_name))
+print(type(full_name))
+print(type(country))
+print(type(city))
+print(type(age))
+print(type(year))
+print(type(is_married))
+print(type(is_true))
+print(type(is_light_on))
 # Print values stored in Variables
 
+print(first_name)
+print(last_name)
+print('full name',full_name)
+print(country)
+print(city)
+print(age)
+print(year)
+print('is married', is_married)
+print('is true', is_true)
+print('is light on', is_light_on)
+
+print('length of first name:', len(first_name))
+print('length of last name:', len(last_name))
+print('length of full name (no spaces):', len(full_name.replace(' ', '')))
